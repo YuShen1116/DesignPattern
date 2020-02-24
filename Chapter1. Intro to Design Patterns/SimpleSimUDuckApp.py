820c14b0f0b67997412acc980772a17eca0951a4
YuShen1116 <darksoul1116@gmail.com>
2020-02-23 21:42:07 -0800

    Add Readme and Simple case

---
+++ Chapter1. Intro to Design Patterns/SimpleSimUDuckApp.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+
+class Duck(ABC):
+
+    def quack(self):
+        # do quack action
+        raise NotImplementedError
+
+    def swim(self):
+        # do swim action
+        raise NotImplementedError
+
+    def fly(self):
+        raise NotImplementedError
+
+    # add other duck like methods
+
+    @abstractmethod
+    def display(self):
+        raise NotImplementedError
+
+
+class MallardDuck(Duck):
+
+    def display(self):
+        print("It looks like a mallard")
+
+
+class RedHeadDuck(Duck):
+
+    def display(self):
+        print("It looks like a red head")
+
+
+class RubberDuck(Duck):
+    
+    def quack(self):
+        # overriden to Squeak
+        pass
+    
+    def fly(self):
+        # do not fly
+        pass
+
+    def display(self):
+        print("It looks like a rubberduck")
+
+
+"""
+The key idea of this design: Inheritance
+
+What's the disadvantage of it?
+
+When you have more and more duck like instance, you'll have to override all the methods you need.
+That makes future maintance become a nightmare
+"""
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DesignPattern
+
+This repo is used to store the python version of examples
