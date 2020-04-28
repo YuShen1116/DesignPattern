ef47aaf9c2b89d728739882bdd241d34afc43704
YuShen1116 <darksoul1116@gmail.com>
2020-04-28 12:36:20 -0700

    Add singleton example

---
+++ Chapter5. The Singleton Pattern/singleton.py
@@ -0,0 +1,47 @@
+# https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+
+
+# Example 1
+def singleton(class_):
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return getinstance
+
+
+@singleton
+class MyClass(object):
+    pass
+
+
+# Example 2
+class Singleton(object):
+    _instance = None
+
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance
+
+
+class MyClass2(Singleton, object):
+    pass
+
+
+# Example 3
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+# Python3
+class MyClass3(object, metaclass=Singleton):
+    pass
